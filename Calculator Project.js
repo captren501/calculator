f9c71d52ac483a5f7a271451c1458df64672c8b5
ethan langford <89049662+captren501@users.noreply.github.com>
2021-11-10 18:09:18 -0700

    added CSS and mostly finish Js
    added CSS and mostly finish Js

--- Calculator Project.js
+++ Calculator Project.js
@@ -13,3 +13,16 @@ function divsion(x,y){
 function mutply(x,y){
     return(x*y)
 }
+
+// temp_1 =""  
+// temp_2 =""
+// // temp_1 = document.getElementById(main).valueS
+// temp_2 =
+function typist(info){
+    document.getElementById('main').value += info
+}
+function answer(){
+ let equation  = document.getElementById('main').value
+ console.log(equation)
+ document.getElementById('main').value = eval(equation)
+}
