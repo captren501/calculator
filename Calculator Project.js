d279e7eb53b5aebacc8ce699712bff77f27a73ab
ethan langford <89049662+captren501@users.noreply.github.com>
2021-11-10 18:23:26 -0700

    css
    more css

--- Calculator Project.js
+++ Calculator Project.js
@@ -1,23 +1,4 @@
-function add(x,y){
-    return(x+y)
-}
 
-function subtract(x,y){
-    return(x-y)
-}
-
-function divsion(x,y){
-    return(X/Y)
-}
-
-function mutply(x,y){
-    return(x*y)
-}
-
-// temp_1 =""  
-// temp_2 =""
-// // temp_1 = document.getElementById(main).valueS
-// temp_2 =
 function typist(info){
     document.getElementById('main').value += info
 }
